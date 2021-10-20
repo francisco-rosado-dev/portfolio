1ca9b6b11b0926f82dd146f117bd9ce68b28cf82
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-20 18:13:30 -0400

    About Styling and resume link

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,10 +3,12 @@ import './about.css'
 function About() {
   return (
     <div>
-      <div className="description">
+      <div className="about">
         <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGu4GDrkMtsAw/profile-displayphoto-shrink_800_800/0/1633041161316?e=1640217600&v=beta&t=7sTDe-zK45sQg2-DxzvgjtTWFr-jzuLkTPbTsuWCsvA" className="profileImage" alt="" />
-        <p>From working in kitchens across NYC to coding web development projects, I have always been focused on finding innovative and creative ways to solve problems. As a software engineer, I look forward to bringing my open mindedness, adaptability, and willingness to accept any challenge to a future team focused on making an impact in the industry. </p>
+        <p className="description">From working in kitchens across NYC to coding web development projects, I have always been focused on finding innovative and creative ways to solve problems. As a software engineer, I look forward to bringing my open mindedness, adaptability, and willingness to accept any challenge to a future team focused on making an impact in the industry. </p>
       </div>
+      <a target="_blank" href="https://drive.google.com/file/d/1fnEgX2b9t4NGHhBwP2Yrey427OVb5aUM/view?usp=sharing">Resume</a>
+
     </div >
   )
 }
