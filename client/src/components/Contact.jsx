a1fca7f5a78c4e24500eecd149f1f77c5ff2c65b
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-20 12:12:01 -0400

    Added more info

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,7 +1,9 @@
 function Contact() {
   return (
     <div>
-      <p>Contact</p>
+      <p>Email: Francisco.Rosado.Dev@gmail.com</p>
+      <p>LinkedIn: https://www.linkedin.com/in/francisco-rosado-dev</p>
+      <p>GitHub: https://github.com/francisco-rosado-dev</p>
     </div>
   )
 }
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,8 +1,9 @@
+import './nav.css'
 import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div>
+    <div className="navigationContent">
       <h1>Francisco Rosado</h1>
       <nav>
         <Link className="link" to="/">Home</Link>
