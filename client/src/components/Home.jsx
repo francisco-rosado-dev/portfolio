b6b29462f65dc0d3b0cb1ffa2f1d8f19a33935cf
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-20 13:10:52 -0400

    Began styling

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,13 @@
+import './home.css'
+
 function Home() {
   return (
     <div>
       <div>
-        <h1>Hello 🌱</h1>
-        <p>I'm a Full-Stack Software Engineer.</p>
-        <p>My stack:</p>
+
+        <h1 className="greet">Hello 🌱</h1>
+        <p className="introduction">Welcome to my portfolio. <br /> I'm a Full-Stack Software Engineer.</p>
+        <p className="stackTitle">Tech stack</p>
         <ul>
           <li>Html</li>
           <li>CSS</li>
@@ -12,6 +15,7 @@ function Home() {
           <li>React.js</li>
           <li>Ruby</li>
           <li>SQL</li>
+          <li>PostgreSQL</li>
           <li>Node</li>
           <li>Express</li>
           <li>MongoDb</li>
@@ -19,7 +23,7 @@ function Home() {
       </div>
       <div>
       </div>
-    </div>
+    </div >
   )
 }
 
