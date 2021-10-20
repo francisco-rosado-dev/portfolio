5f2b124604afebfb3f6fdc7b2837ddf65a7252ba
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-20 17:29:12 -0400

    Styled Home and Nav components

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,22 +6,19 @@ function Home() {
       <div>
         <h1 className="greet">Hello 🌱</h1>
         <p className="introduction">Welcome to my portfolio. <br /> I am a New York based Full-Stack Software Engineer.</p>
-        <p className="stackTitle">Tech stack</p>
-        <ul>
-          <li>Html</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React.js</li>
-          <li>Ruby</li>
-          <li>Rails</li>
-          <li>SQL</li>
-          <li>PostgreSQL</li>
-          <li>Node</li>
-          <li>Express</li>
-          <li>MongoDb</li>
+        <p className="stackTitle">Tech Stack</p>
+        <ul className="stackItems">
+          <li className="stackItem">Html</li>
+          <li className="stackItem">CSS</li>
+          <li className="stackItem">JavaScript</li>
+          <li className="stackItem">React.js</li>
+          <li className="stackItem">Ruby</li>
+          <li className="stackItem">SQL</li>
+          <li className="stackItem">PostgreSQL</li>
+          <li className="stackItem">Node</li>
+          <li className="stackItem">Express</li>
+          <li className="stackItem">MongoDb</li>
         </ul>
-      </div>
-      <div>
       </div>
     </div >
   )
--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 export default function Nav() {
   return (
     <div className="navigationContent">
-      <h1>Francisco Rosado</h1>
-      <nav>
+      <h1 className="name">Francisco Rosado</h1>
+      <nav className="navigationContainer">
         <NavLink className="link" to="/">Home</NavLink>
         <NavLink className="link" to="/about">About</NavLink>
         <NavLink className="link" to="/projects">Projects</NavLink>
