125227587532fd5ef28c03f36b491372da0f83a3
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-22 16:37:13 -0400

    updated stack

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,19 +5,26 @@ function Home() {
     <div>
       <div>
         <h1 className="greet">Hello 🌱</h1>
-        <p className="introduction">Welcome to my portfolio. <br /> I am a New York based Full-Stack Software Engineer.</p>
+        <p className="introduction">Welcome to my portfolio. <br /> I am a New York based Software Engineer.</p>
         <p className="stackTitle">Tech Stack</p>
         <ul className="stackItems">
           <li className="stackItem">Html</li>
           <li className="stackItem">CSS</li>
           <li className="stackItem">JavaScript</li>
-          <li className="stackItem">React.js</li>
+          <li className="stackItem">Bootstrap</li>
+          <li className="stackItem">React</li>
+          <li className="stackItem">Node</li>
+          <li className="stackItem">jQuery</li>
+          <li className="stackItem">Express</li>
+          <li className="stackItem">Mongoose</li>
           <li className="stackItem">Ruby</li>
+          <li className="stackItem">Rails</li>
           <li className="stackItem">SQL</li>
           <li className="stackItem">PostgreSQL</li>
-          <li className="stackItem">Node</li>
-          <li className="stackItem">Express</li>
-          <li className="stackItem">MongoDb</li>
+          <li className="stackItem">MongoDB</li>
+          <li className="stackItem">Postman API</li>
+          <li className="stackItem">Git</li>
+          <li className="stackItem">GitHub</li>
         </ul>
       </div>
     </div >
