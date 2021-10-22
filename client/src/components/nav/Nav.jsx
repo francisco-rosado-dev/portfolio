cdc4ca0f5eac462ca59c540d6b2fd86724d955e8
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-22 18:26:06 -0400

    implemented NavLink activeClass selector

--- client/src/components/nav/Nav.jsx
+++ client/src/components/nav/Nav.jsx
@@ -2,14 +2,17 @@ import './nav.css'
 import { NavLink } from "react-router-dom";
 
 export default function Nav() {
+
   return (
     <div className="navigationContent">
       <h1 className="name">Francisco Rosado</h1>
       <nav className="navigationContainer">
-        <NavLink className="link" to="/">Home</NavLink>
-        <NavLink className="link" to="/about">About</NavLink>
-        <NavLink className="link" to="/projects">Projects</NavLink>
-        <NavLink className="link" to="/contact">Contact</NavLink>
+
+        <NavLink className="dashboard-selector" activeClassName='active-selector' exact to="/">Home</NavLink>
+        <NavLink className="dashboard-selector" activeClassName='active-selector' to="/about">About</NavLink>
+        <NavLink className="dashboard-selector" activeClassName='active-selector' to="/projects">Projects</NavLink>
+        <NavLink className="dashboard-selector" activeClassName='active-selector' to="/contact" >Contact</NavLink>
+
       </nav>
     </div>
   )
