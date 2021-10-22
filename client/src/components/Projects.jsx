f6227af8942262c7e60e4bdca81bc4ded3a2f904
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-22 12:07:39 -0400

    Added three projects to projects section

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import './projects.css'
 import humancoin from '.././humancoin.png'
+import ysfj from '.././ysfj.png'
+import musicphile from '.././musicphile.jpg'
 
 function Work() {
   return (
@@ -7,6 +9,20 @@ function Work() {
       <div className="project">
         <img src={humancoin} />
         <h3>Human Coin</h3>
+        <p>Human Coin is a React application that introduces the innovative concepts of cryptocurrencies to manual labor. By tapping or clicking on the coin found in your wallet, you chip closer to finding a coin.</p>
+        <a target="_blank" href="https://drive.google.com/file/d/1fnEgX2b9t4NGHhBwP2Yrey427OVb5aUM/view?usp=sharing"><button type="button" className="resume">GitHub</button></a>
+        <a target="_blank" href="https://drive.google.com/file/d/1fnEgX2b9t4NGHhBwP2Yrey427OVb5aUM/view?usp=sharing"><button type="button" className="resume">Live Site</button></a>
+      </div>
+      <div className="project">
+        <img src={ysfj} />
+        <h3>YSFJ</h3>
+        <p>Human Coin is a React application that introduces the innovative concepts of cryptocurrencies to manual labor. By tapping or clicking on the coin found in your wallet, you chip closer to finding a coin.</p>
+        <a target="_blank" href="https://github.com/yeana-dev/YSFJ/tree/main"><button type="button" className="resume">GitHub</button></a>
+        <a target="_blank" href="https://https://ysfj.netlify.app/"><button type="button" className="resume">Live Site</button></a>
+      </div>
+      <div className="project">
+        <img src={musicphile} />
+        <h3>Musicphile</h3>
         <p>Human Coin is a React application that introduces the innovative concepts of cryptocurrencies to manual labor. By tapping or clicking on the coin found in your wallet, you chip closer to finding a coin.</p>
         <a target="_blank" href="https://drive.google.com/file/d/1fnEgX2b9t4NGHhBwP2Yrey427OVb5aUM/view?usp=sharing"><button type="button" className="resume">GitHub</button></a>
         <a target="_blank" href="https://drive.google.com/file/d/1fnEgX2b9t4NGHhBwP2Yrey427OVb5aUM/view?usp=sharing"><button type="button" className="resume">Live Site</button></a>
