81f2b987612754c8eeb4b96cdcb26288a507695b
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-26 12:03:20 -0400

    Links

--- client/src/screens/projects/Projects.jsx
+++ client/src/screens/projects/Projects.jsx
@@ -10,15 +10,15 @@ function Work() {
         <img alt="Human Coin" src={humancoin} className="portfolioImages" />
         <h3>Human Coin</h3>
         <p>Human Coin is a React application that introduces the innovative concepts of cryptocurrencies to manual labor. By tapping or clicking on the coin found in your wallet, you chip closer to finding a coin.</p>
-        <a target="_blank" rel="noreferrer" href="https://https://github.com/francisco-rosado-dev/human-coin"><button type="button" className="portfolioLinks">GitHub</button></a>
-        <a target="_blank" rel="noreferrer" href="https://https://human-coin.netlify.app/"><button type="button" className="portfolioLinks">Live Site</button></a>
+        <a target="_blank" rel="noreferrer" href="https://github.com/francisco-rosado-dev/human-coin"><button type="button" className="portfolioLinks">GitHub</button></a>
+        <a target="_blank" rel="noreferrer" href="https://human-coin.netlify.app/"><button type="button" className="portfolioLinks">Live Site</button></a>
       </div>
       <div className="project">
         <img alt="YSFJ" src={ysfj} className="portfolioImages" />
         <h3>YSFJ</h3>
         <p>From the creators of the famous blog 'four-eyes', YSFJ is a streamlined virtual eyewear boutique. Don't lose sight of what matters most. Glasses for adventure.</p>
         <a target="_blank" rel="noreferrer" href="https://github.com/yeana-dev/YSFJ/tree/main"><button type="button" className="portfolioLinks">GitHub</button></a>
-        <a target="_blank" rel="noreferrer" href="https://https://ysfj.netlify.app/"><button type="button" className="portfolioLinks">Live Site</button></a>
+        <a target="_blank" rel="noreferrer" href="https://ysfj.netlify.app/"><button type="button" className="portfolioLinks">Live Site</button></a>
       </div>
       <div className="project">
         <img alt="musicphile" src={musicphile} className="portfolioImages" />
