16fd81d081b1d3e5af9582f73e7010c26dfb361d
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-10-26 17:49:58 -0400

    Styling for home page, projects, and contact.

--- client/src/screens/contact/Contact.jsx
+++ client/src/screens/contact/Contact.jsx
@@ -1,10 +1,16 @@
+import github from "./github.jpg"
+import linkedin from "./linkedin.png"
+import "./contact.css"
+
 function Contact() {
   return (
     <div>
       <p>Email: Francisco.Rosado.Dev@gmail.com</p>
-      <p>LinkedIn: https://www.linkedin.com/in/francisco-rosado-dev</p>
-      <p>GitHub: https://github.com/francisco-rosado-dev</p>
-    </div>
+      <a href="https://www.linkedin.com/in/francisco-rosado-dev" target="_blank" rel="noreferrer"><img className="contactLink" src={linkedin} alt="LinkedIn" /></a>
+      <br />
+      <a href="https://github.com/francisco-rosado-dev" target="_blank" rel="noreferrer"><img className="contactLink" src={github} alt="GitHub" /></a>
+    </div >
+
   )
 }
 
--- client/src/screens/projects/Projects.jsx
+++ client/src/screens/projects/Projects.jsx
@@ -9,21 +9,21 @@ function Work() {
       <div className="project">
         <img alt="Human Coin" src={humancoin} className="portfolioImages" />
         <h3>Human Coin</h3>
-        <p>Human Coin is a React application that introduces the innovative concepts of cryptocurrencies to manual labor. By tapping or clicking on the coin found in your wallet, you chip closer to finding a coin.</p>
+        <p class="projectDescription">Human Coin is a React application that introduces the innovative concepts of cryptocurrencies to manual labor. By tapping or clicking on the coin found in your wallet, you chip closer to finding a coin.</p>
         <a target="_blank" rel="noreferrer" href="https://github.com/francisco-rosado-dev/human-coin"><button type="button" className="portfolioLinks">GitHub</button></a>
         <a target="_blank" rel="noreferrer" href="https://human-coin.netlify.app/"><button type="button" className="portfolioLinks">Live Site</button></a>
       </div>
       <div className="project">
         <img alt="YSFJ" src={ysfj} className="portfolioImages" />
         <h3>YSFJ</h3>
-        <p>From the creators of the famous blog 'four-eyes', YSFJ is a streamlined virtual eyewear boutique. Don't lose sight of what matters most. Glasses for adventure.</p>
+        <p class="projectDescription">From the creators of the famous blog 'four-eyes', YSFJ is a streamlined virtual eyewear boutique. Don't lose sight of what matters most. Glasses for adventure.</p>
         <a target="_blank" rel="noreferrer" href="https://github.com/yeana-dev/YSFJ/tree/main"><button type="button" className="portfolioLinks">GitHub</button></a>
         <a target="_blank" rel="noreferrer" href="https://ysfj.netlify.app/"><button type="button" className="portfolioLinks">Live Site</button></a>
       </div>
       <div className="project">
         <img alt="musicphile" src={musicphile} className="portfolioImages" />
         <h3>Musicphile</h3>
-        <p>Musicphile is a music sharing library. Share what you find with the world, and amuse yourself with arcane tunes brought to you by other music enthusiasts.</p>
+        <p class="projectDescription">Musicphile is a music sharing library. Share what you find with the world, and amuse yourself with arcane tunes brought to you by other music enthusiasts.</p>
         <a target="_blank" rel="noreferrer" href="https://github.com/francisco-rosado-dev/musicphile"><button type="button" className="portfolioLinks">GitHub</button></a>
         <a target="_blank" rel="noreferrer" href="https://musicphile-app.netlify.app/"><button type="button" className="portfolioLinks">Live Site</button></a>
       </div>
