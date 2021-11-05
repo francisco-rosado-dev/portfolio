337acf58d71274df78508e67cb9fdd38addd2ec5
FranciscoPink <francisco.rosado.dev@gmail.com>
2021-11-05 16:54:33 -0400

    Updated image URLs

--- client/src/screens/contact/Contact.jsx
+++ client/src/screens/contact/Contact.jsx
@@ -1,14 +1,12 @@
-import github from "./github.jpg"
-import linkedin from "./linkedin.png"
 import "./contact.css"
 
 function Contact() {
   return (
     <div>
       <p>Email: Francisco.Rosado.Dev@gmail.com</p>
-      <a href="https://www.linkedin.com/in/francisco-rosado-dev" target="_blank" rel="noreferrer"><img className="contactLink" src={linkedin} alt="LinkedIn" /></a>
+      <a href="https://www.linkedin.com/in/francisco-rosado-dev" target="_blank" rel="noreferrer"><img src="https://lh3.googleusercontent.com/W2LoL54qiKVHzeU5gaM620ORBBrLQm1a-hkCIO7i43x-PNkteA-Bx0Xl0zZu2V6-7focebOXcomAxKoPyPISedtuxXP0sBLqOTH53yCnDYlRy3KjnAAFyVB3j4-5a1BtD2a4Hjy-hwmh7qSR3oSAQFvRmCF5PxeGoomC9nXQVTgkWSVkp-imm-aGrAx4myNeXnzRUD5-ZkqRGxcf-a-bbRUDrfiilzXCjbHN3YXXX74YKrrYhwkz5m6XdVqIWHIoU2lcYmY2fIsxbu0bzyH_NJrWVjwCIYsaT3BPd6GzRbWcOJ41qEngUM_ZTv1mbZCQEhRZjIiiF5OK0uaGyjPWCjHFCbXGZJGF_2fvYh2yPkx7NoZGNc4zEvRMi8dnrUiGKvbxGzKC9Y3DPAYWT1Yw24rPlFaUf0Gq-2PJQgblf8hKVb_u9pic-goETo8hx7i79-Iw9JmCbGIDbMdODryUVP3DN_t1Fhw26mPBUkj_Si_yIalY1-mdGXxi2DSMV2xsXC92M1tUgDa_EXXGM0WDib_HJiQ7XJKSU1PpgDy7VIvbQ1j9s9AcU9OWc6XI53bShQs4CVozz2tuOZC1Taj1bavwysgYDzp0-eDumpCZZc1bmpq_MGoQQbgnQrllBWpbjMfpmwxEjmlZlZB9vCjb5Mbk-Z2n_P2OiYc6j4E-8t38CeDw0AyrlfKZT1WR1gaXvKjue7ZB_LpZcRZXrXbhkg=s225-no?authuser=0" className="contactLink" alt="LinkedIn" /></a>
       <br />
-      <a href="https://github.com/francisco-rosado-dev" target="_blank" rel="noreferrer"><img className="contactLink" src={github} alt="GitHub" /></a>
+      <a href="https://github.com/francisco-rosado-dev" target="_blank" rel="noreferrer"><img src="https://lh3.googleusercontent.com/9J9JI6EYuE70lSyoy7GRIDfb6bBaZS2L8TEUtNgJar5O9MZyZYzDqegKMrXrp6x5pSwHOpdlJsOTrY7FJorQ5ZJHcXkXlEOLonTR9P6b9zR1dQV14v3uMsqLQXEEvwJGll_v1Stu08aCoWGHIRuBYuhSZGwpgdDGRwQf2hctMJ2RZfPXUfR8aYQlchhFRf_lxHl0whk1H44wy3zl8FFSOud4czMsoRx6lXwV51NoG0iLy8EbtwPY2ukPqWW0qhnrZZ6M6jEoIVz__xRfxjkmGos7-42pCH9KtlQZ4fk754FhqbRXZvIYfcw0Qc8oxOcYyLxlHHC0Es7sn3whH0lD1cnnT2MnC5_5M88bQCt0fFDe34XNHMs3N6b20p-JiLgCTbrIKTFd1OlhUIPjRDeCWrgdQwGWb3hilgjISwTO0pJH6S3ITpOU90qtnxccf5hGuhi2HbiWs4zTMJoAYbUw25V95PL3NKdIsdDyiPQ05y--mXvW58zwfCe4wyAKsUvJwfOtjeL6YNyI6Cjlu8epCMZOcXzy_y5qXFu1qx_saLBW8YubZngNYZNGDj_SwcAgYsDwpOysrbYJFlFjG6L9ZKsDhvv5InWvLjQO6lYu2WFD1QaHH3YN_KvGvWfr8yuUEXcRyUd7aCBElZhBTIHPJ0smFOyrTef0p_XdGaD76Ol4TOnhbqq813hyFQHLVS5hmyQ_RFg2iyZgQP9BuNryaw=s1592-no?authuser=0" className="contactLink" alt="GitHub" /></a>
     </div >
 
   )
