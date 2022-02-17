79611eb2202a89b4b58d46e20d9a3db4314724f2
FranciscoPink <francisco.rosado.dev@gmail.com>
2022-02-17 13:38:46 -0500

    Updated Links

--- client/src/screens/about/About.jsx
+++ client/src/screens/about/About.jsx
@@ -4,10 +4,10 @@ function About() {
   return (
     <div>
       <div className="about">
-        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGu4GDrkMtsAw/profile-displayphoto-shrink_800_800/0/1633041161316?e=1648080000&v=beta&t=KuMsj4qONCXKnQFuI2eGsLmoSJE_xSsn-BMf3DLOSn0" className="profileImage" alt="" />
+        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGUaQmnEbo8xw/profile-displayphoto-shrink_800_800/0/1643925148868?e=1650499200&v=beta&t=P8jlY3_IR1apbqRWASbtr10rgZ2BXdxItk-byzX0efM" className="profileImage" alt="" />
         <p className="description">From working in kitchens across NYC to coding web development projects, I have always been focused on finding innovative and creative ways to solve problems. As a software engineer, I look forward to bringing my open mindedness, adaptability, and willingness to accept any challenge to a future team focused on making an impact in the industry. </p>
       </div>
-      <a target="_blank" rel="noreferrer" href="hhttps://drive.google.com/file/d/106oTXxC6SiKXVaFzJJyS2VJHRJ6f9Gc_/view?usp=sharing"><button type="button" className="resume">Resume</button></a>
+      <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/106oTXxC6SiKXVaFzJJyS2VJHRJ6f9Gc_/view?usp=sharing"><button type="button" className="resume">Resume</button></a>
 
     </div >
   )
