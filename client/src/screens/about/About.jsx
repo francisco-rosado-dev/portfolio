49762aec950a1dcdd9237aae3f7776d6bb2b417b
francisco-rosado-dev <francisco.rosado.dev@gmail.com>
2022-05-11 14:31:00 -0400

    Updated Resume Link

--- client/src/screens/about/About.jsx
+++ client/src/screens/about/About.jsx
@@ -7,7 +7,7 @@ function About() {
         <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGUaQmnEbo8xw/profile-displayphoto-shrink_800_800/0/1643925148868?e=1657756800&v=beta&t=Ezkf7dawSNJhL54CIA2_MH4CoE5pdwmcKq3HXiNh9vQ" className="profileImage" alt="" />
         <p className="description">With a background in customer service, hospitality, and administration, I have always focused on finding innovative and creative ways to solve problems. I look forward to bringing my open-mindedness, empathy, curiosity, and willingness to accept any challenge to a future team focused on making an impact.</p>
       </div>
-      <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/106oTXxC6SiKXVaFzJJyS2VJHRJ6f9Gc_/view?usp=sharing"><button type="button" className="resume">Resume</button></a>
+      <a target="_blank" rel="noreferrer" href="https://drive.google.com/file/d/19PD6ozeEZtJrR3FnQJuXegV6rOx_V90N/view?usp=sharing"><button type="button" className="resume">Resume</button></a>
 
     </div >
   )
--- client/src/screens/contact/Contact.jsx
+++ client/src/screens/contact/Contact.jsx
@@ -3,7 +3,7 @@ import "./contact.css"
 function Contact() {
   return (
     <div>
-      <p>Email: Francisco.Rosado.Dev@gmail.com</p>
+      <a href="mailto:Francisco.Rosado.Dev@gmail.com"><p>Francisco.Rosado.Dev@gmail.com</p></a>
       <a href="https://www.linkedin.com/in/francisco-rosado-dev" target="_blank" rel="noreferrer"><img src="https://lh3.googleusercontent.com/KeuRmcnGbD9jQdMBVS_o_LGrxeepYp36AV-d-rG-rZHOza3aXlpCJEGJ28GrIbET32zpsfIgLe4oQBiEhy-BS9v4vyr60lGN0EfUVsPX57qLVw8i4CpwlgYsGVeT2tsdRgvhLRTb=s225-p-k" className="contactLink" alt="LinkedIn" /></a>
       <br />
       <a href="https://github.com/francisco-rosado-dev" target="_blank" rel="noreferrer"><img src="https://lh3.googleusercontent.com/A58taIcFLggr62Mee_hwtRUWnm67oAJieEa-6OcOwaucsejxWBMTL4MOKzmaCrbJfeA9RwlSsyZowgQzhu38z_pb-TA1wrOHuv9G5uPJ_rvPpbwgh_oUob0B2pk2o2uJNJI6zoGa=w2400" className="contactLink" alt="GitHub" /></a>
